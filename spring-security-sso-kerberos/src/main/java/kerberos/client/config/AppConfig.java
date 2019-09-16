600d30ab3dfde77ef06610fc94a6755a7f93cbae
Gonzalo Orellano <gonzalo.orellano@globallogic.com>
2019-09-16 11:22:14 -0300

    Add new module

---
+++ spring-security-sso-kerberos/src/main/java/kerberos/client/config/AppConfig.java
@@ -0,0 +1,9 @@
+package kerberos.client.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(KerberosConfig.class)
+public class AppConfig {
+}
